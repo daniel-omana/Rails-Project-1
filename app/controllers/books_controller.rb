50ee7b770a61d1677c6d9f1624d22f2deb7284ef
Daniel <danielomana@shaw.ca>
2020-10-26 03:18:42 -0500

    Finished implementing the show and index views for books and publisher.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,9 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.includes(:publisher).order('rating DESC')
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'publishers/index'
-  get 'publishers/show'
-  get 'books/index'
-  get 'books/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :books, only: %i[index show]
+  resources :publishers, only: %i[index show]
 end
