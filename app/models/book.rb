ca2e5cbc496c8fecfd705e671df86ab050d17549
Daniel <danielomana@shaw.ca>
2020-10-26 02:25:50 -0500

    Created controller for Movie

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :publisher
 
-  vallidats :title, :isbn, :isbn13, :number_of_pages, :rating, presence: true
+  validates :title, :isbn, :isbn13, :number_of_pages, :rating, presence: true
   validates :title, uniqueness: true
   validates :isbn, :isbn13, :number_of_pages, numericality: { only_integer: true }
   validates :rating, numericality: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,30 @@
 require 'faker'
 require 'csv'
+
+Book.delete_all
+Publisher.delete_all
+
+csv_data = File.read(Rails.root.join('db/books.csv'))
+books = CSV.parse(csv_data, headers: true, encoding: 'utf-8')
+
+books.each do |b|
+  publisher = Publisher.find_or_create_by(name: b['publisher'])
+
+  if publisher&.valid?
+    # create the books
+    book = publisher.books.create(
+      title: b['title'],
+      isbn: b['isbn'],
+      isbn13: b['isbn13'],
+      number_of_pages: b['num_pages'],
+      rating: b['average_rating']
+    )
+
+    puts "Invalid book #{b['title']}" unless book&.valid?
+  else
+    puts "Invalid publisher #{m['publisher']} for book #{b['title']}"
+  end
+end
+
+puts "Created #{Publisher.count} publishers"
+puts "Created #{Book.count} books"
