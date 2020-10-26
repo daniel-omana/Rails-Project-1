494df732eb86c47f89c45ba145080078728a35c1
Daniel <danielomana@shaw.ca>
2020-10-26 02:26:14 -0500

    Created controller for Publisher Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'publishers/index'
+  get 'publishers/show'
   get 'books/index'
   get 'books/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
